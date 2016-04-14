7420838bb9cdc399be2d40da59627279641a3a1f
na001604 <panzhiyang@cn.fujitsu.com>
2016-04-14 18:54:35 +0800

    好

--- src/com/jftt/CopyContent.java
+++ src/com/jftt/CopyContent.java
@@ -36,7 +36,7 @@ public class CopyContent {
 				}
 				
 			}
-			System.out.print("success");
+			System.out.print("successful");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
